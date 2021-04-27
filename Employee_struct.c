b9cadda97d1ece206e47ea5b40366468e9d31049
Lohetha <80204669+Lohetha@users.noreply.github.com>
2021-04-27 20:32:35 +0530

    c file

--- Employee_struct.c
+++ Employee_struct.c
@@ -5,12 +5,12 @@ struct employee{
     char emp_name[30]; 
     char dept_name[20]; 
     unsigned int salary;
-}e[3];
+}e[20];
 
 void display(){
     int i;
     printf("Emp_number   Emp_name   Dept_name   Salary\n");
-    for(i=0;i<3;i++){
+    for(i=0;i<20;i++){
       printf("%i   %s   %s   %i\n",e[i].emp_no,e[i].emp_name,e[i].dept_name,e[i].salary);
     }
 }
@@ -18,7 +18,7 @@ void display(){
 int main()
 {   int i;
     printf("Enter employee details:");
-    for(i=0;i<3;i++){
+    for(i=0;i<20;i++){
         scanf("%i%s%s%i",&e[i].emp_no,e[i].emp_name,e[i].dept_name,&e[i].salary);
     }
     display(e);
